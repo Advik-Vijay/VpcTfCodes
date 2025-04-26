e7d7fd11ed9aefea49b540360a1015161687ac5d
Advik-Vijay <advikvijay7092@gmail.com>
2025-04-27 00:47:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-01e479df1702f1d13"  #Ubuntu
+  ami               = "ami-0f9de6e2d2f067fca"  #Ubuntu
   #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
-  availability_zone = "eu-west-2a"
-  #availability_zone = "eu-west-2b"
+  availability_zone = "us-east-1a"
   tags = {
-     "Name" = "web"
-    #"tag1" = "Update-test-1"    
+     "Name" = "web"   
   }
 }
 
